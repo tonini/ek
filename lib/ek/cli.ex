08173e81acfcd5b3d7b2999a69a4a592773cb128
Samuel Tonini <tonini.samuel@gmail.com>
2015-01-05 15:42:52 +0100

    basic CLI

---
+++ lib/ek/cli.ex
@@ -0,0 +1,33 @@
+defmodule Ek.CLI do
+
+  def run(argv) do
+    parse_args(argv)
+    |> process
+  end
+
+  def parse_args(argv) do
+    parse = OptionParser.parse(argv,
+                               switches: [ help: :boolean],
+                               aliases:  [ h:    :help])
+
+    case parse do
+      { [ help: true ] } -> :help
+      _ -> :help
+    end
+  end
+
+  def process(:help) do
+    IO.write """
+    usage:
+      ek new PATH [OPTIONS]
+
+    Options:
+      -b, --bin   Generate a binary directory for your package
+      -t, --test  Generate a test directory for your package
+
+    Creates a skeleton for creating an emacs package
+    """
+    System.halt(0)
+  end
+
+end
--- test/ek_test.exs
+++ test/ek_test.exs
@@ -1,8 +1,24 @@
 defmodule EkTest do
   use ExUnit.Case
-  doctest Ek
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  import Ek.CLI, only: [ parse_args: 1, run: 1 ]
+
+  test ":help returned by option parsing with -h and --help options" do
+    assert parse_args(["-h"])     == :help
+    assert parse_args(["--help"]) == :help
   end
+
+  test "process :help and return an usage message" do
+    assert run(["-h"]) == """
+    usage:
+      ek new PATH [OPTIONS]
+
+    Options:
+      -b, --bin   Generate a binary directory for your package
+      -t, --test  Generate a test directory for your package
+
+    Creates a skeleton for creating an emacs package
+    """
+  end
+
 end
